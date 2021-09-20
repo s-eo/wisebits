8c010d4fc6b78cb476760e5afb0c01fc92d9836e
eos <eoskakovskaya@mail.ru>
2021-09-20 19:36:12 +0300

    Adding HighlihgtText tests

--- src/components/HighlightText/HighlightText.test.tsx
+++ src/components/HighlightText/HighlightText.test.tsx
@@ -1,23 +1,99 @@
-import React from "react";
+import React, {FunctionComponent} from "react";
 import {cleanup, render} from '@testing-library/react';
 import {HighlightText} from "./index";
 
 
 afterEach(cleanup);
 
-it('With-prop wraps text item in the children',  () => {
-  const expected = [
-    { 'nodeName': '#text', 'textContent': 'foo'},
-    { 'nodeName': '#text', 'textContent': ' '},
-    { 'nodeName': 'SPAN', 'textContent': 'bar'},
-    { 'nodeName': '#text', 'textContent': ' '},
-    { 'nodeName': '#text', 'textContent': 'baz'},
-  ];
+describe('Returns children as is', () => {
+  it('when with-prop is empty',  () => {
+    const {container} = render(<HighlightText with="" text={['bar']}>foo bar baz</HighlightText>);
 
-  const {container} = render(<HighlightText with="span" text={['bar']}>foo bar baz</HighlightText>);
+    expect(container?.firstChild?.nodeName).toMatch('#text');
+    expect(container?.firstChild?.textContent).toMatch( 'foo bar baz');
+  });
 
-  for (let [index, item] of container.childNodes.entries()) {
-    expect(item.nodeName).toMatch(expected[index].nodeName);
-    expect(item.textContent).toMatch(expected[index].textContent);
-  }
+  it('when text-array is empty',  () => {
+    const {container} = render(<HighlightText with="span" text={[]}>foo bar baz</HighlightText>);
+
+    expect(container?.firstChild?.nodeName).toMatch('#text');
+    expect(container?.firstChild?.textContent).toMatch( 'foo bar baz');
+  });
+
+  it('when they are not a string',  () => {
+    const Cat = () => <div>Cat</div>;
+    const {container} = render(<HighlightText with="span" text={['Cat']}>
+      <Cat/>
+      <Cat/>
+    </HighlightText>);
+
+    expect(container.firstChild?.textContent).toBe('Cat');
+    expect(container.lastChild?.textContent).toBe('Cat');
+  });
+});
+
+it('Doesn\'t fail on empty children',  () => {
+  const {container} = render(<HighlightText with="span" text={['bar']} />);
+
+  expect(container.firstChild).toBe(null);
+});
+
+describe('With-prop wraps', () => {
+  it('text item in the children',  () => {
+    const expected = [
+      { 'nodeName': '#text', 'textContent': 'foo'},
+      { 'nodeName': '#text', 'textContent': ' '},
+      { 'nodeName': 'SPAN', 'textContent': 'bar'},
+      { 'nodeName': '#text', 'textContent': ' '},
+      { 'nodeName': '#text', 'textContent': 'baz'},
+    ];
+
+    const {container} = render(<HighlightText with="span" text={['bar']}>foo bar baz</HighlightText>);
+
+    for (let [index, item] of container.childNodes.entries()) {
+      expect(item.nodeName).toMatch(expected[index].nodeName);
+      expect(item.textContent).toMatch(expected[index].textContent);
+    }
+  });
+
+  it('multiple text items in the children',  () => {
+    const expected = [
+      { 'nodeName': '#text', 'textContent': 'foo'},
+      { 'nodeName': '#text', 'textContent': ' '},
+      { 'nodeName': 'B', 'textContent': 'bar'},
+      { 'nodeName': '#text', 'textContent': ' '},
+      { 'nodeName': 'B', 'textContent': 'baz'},
+      { 'nodeName': '#text', 'textContent': ' '},
+      { 'nodeName': 'B', 'textContent': 'bar'},
+    ];
+
+    const {container} = render(<HighlightText with="b" text={['bar', 'baz']}>foo bar baz bar</HighlightText>);
+
+    for (let [index, item] of container.childNodes.entries()) {
+      expect(item.nodeName).toMatch(expected[index].nodeName);
+      expect(item.textContent).toMatch(expected[index].textContent);
+    }
+  });
+
+  it('with non-string component',  () => {
+    interface CoatProps {
+      children?: React.ReactNode;
+    }
+    const Coat: FunctionComponent<CoatProps> = (props: CoatProps) => <>{props.children} in a warm coat</>;
+    const expected = [
+      { 'nodeName': '#text', 'textContent': 'foo'},
+      { 'nodeName': '#text', 'textContent': ' '},
+      { 'nodeName': '#text', 'textContent': 'bar'},
+      { 'nodeName': '#text', 'textContent': 'in a warm coat'},
+      { 'nodeName': '#text', 'textContent': ' '},
+      { 'nodeName': '#text', 'textContent': 'baz'},
+    ];
+
+    const {container} = render(<HighlightText with={Coat} text={['bar']}>foo bar baz</HighlightText>);
+
+    for (let [index, item] of container.childNodes.entries()) {
+      expect(item.nodeName).toMatch(expected[index].nodeName);
+      expect(item.textContent).toMatch(expected[index].textContent);
+    }
+  });
 });
--- src/components/HighlightText/index.tsx
+++ src/components/HighlightText/index.tsx
@@ -1,22 +1,26 @@
 import React, {FC, ReactNode, useMemo} from 'react';
 
-interface IProps {
+interface IHighlightProps {
   with: FC | string;
   text: string[];
   children?: ReactNode;
 }
 
-export const HighlightText = (props:IProps) => {
+interface IMemoHighlightProps {
+  text: string
+}
+
+export const HighlightText = (props:IHighlightProps) => {
   // remove duplicates and use effectively with Set
   const templates: Set<string> = useMemo(() => new Set(props.text), [props.text]);
 
   // if wrapping prop is an empty string,
   // if template array prop is empty,
   // if no children provided - return as is
-  if (!props.with || !templates.size || !props.children) return <>props.children</> || null;
+  if (!props.with || !templates.size || !props.children) return <>{props.children}</>;
 
   // can`t search template in non-string children. Return as is
-  if (typeof props.children !== 'string') return <>props.children</> || null;
+  if (typeof props.children !== 'string') return <>{props.children}</>;
 
   const With = props.with;
 
@@ -27,7 +31,7 @@ export const HighlightText = (props:IProps) => {
       .map((node, index, all) => {
         const isLast = index === all.length - 1;
         const ElementType = templates.has(node) ? With : React.Fragment;
-        const PureNode = React.memo(({ text }: {text: string}) => <ElementType>{text}</ElementType>);
+        const PureNode = React.memo((props: IMemoHighlightProps) => <ElementType>{props.text}</ElementType>);
         const NewNode = <PureNode text={node} key={`${node + '_' + index}`}/>;
 
         return [NewNode, isLast ? null : ' '];
